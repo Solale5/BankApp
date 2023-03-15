06d723f1ee357e2a962e129a8d5ccb966a1da4aa
AhmedHodhod <alhdhody@gmail.com>
2023-03-14 22:08:38 -0700

    DB connection is working

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,8 @@ app.get('/', function (req, res) {
     res.send("Hello World!")
 });
 
-app.listen(5432, function () {
-    console.log("App listening on port 5432");
+app.listen(3000, function () {
+    console.log("App listening on port 3000");
     connection.connect(function (err) {
         if (err) throw err;
         console.log("Database connected!")
