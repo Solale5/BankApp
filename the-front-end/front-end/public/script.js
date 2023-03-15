ba456189a66bd5b1d10c472fd3bc794aba293b2e
HarrisonHwang <78999886+HarrisonHwang@users.noreply.github.com>
2023-03-15 15:20:51 -0700

    changed toggle method
    changed toggle method to toggle whole <div> instead of each individual item

--- the-front-end/front-end/public/script.js
+++ the-front-end/front-end/public/script.js
@@ -1,23 +1,11 @@
 function transferChecking() {
-  var text = document.getElementById("text");
-  text.innerHTML = "tranfer to:";
-  text.classList.toggle("hide");
-  text.classList.toggle("show");
 
+  var toggle = document.getElementById("toggleChecking");
+  if(toggleChecking.style.display === "block"){
+    toggleChecking.style.display = "none";
+  } else {
+    toggleChecking.style.display = "block";
+  }
 
-  var drop = document.getElementById("Checkingdrop");
-  drop.classList.toggle("hide");
-  drop.classList.toggle("show");
-
-  var field = document.getElementById("textbox");
-  field.classList.toggle("hide");
-  field.classList.toggle("show");
-
-  var go = document.getElementById("confirm");
-  go.classList.toggle("hide");
-  go.classList.toggle("show");
 
 }
-
-//redo so the a whole div gets hidden
-// /https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
