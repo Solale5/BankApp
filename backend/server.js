688dbb1ba4e263353e01de89c83d5afb838122a0
Solale5 <solomon.alemu@sjsu.edu>
2023-03-29 10:24:47 -0700

    register feature, added nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,85 +1,91 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config()
+  require("dotenv").config();
 }
 
- 
 // Importing Libraies that we installed using npm
-const express = require("express")
-const app = express()
-const bcrypt = require("bcrypt") // Importing bcrypt package to encrypt the password
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt"); // Importing bcrypt package to encrypt the password
 // to login using goole or facebook or ..
-const passport = require("passport")
-const initializePassport = require("./passport-config")
+const passport = require("passport");
+const initializePassport = require("./passport-config");
 // to disply some flash messages such as "You are logged in"
-const flash = require("express-flash")
-const session = require("express-session")
-const methodOverride = require("method-override")
-
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
 initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-    )
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
-const users = []
-// send the form data to the server in the request body not as a query string 
-app.use(express.urlencoded({extended: false}))
+const users = [];
+// send the form data to the server in the request body not as a query string
+app.use(express.urlencoded({ extended: false }));
 
-app.use(flash())
+app.use(flash());
 
-
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false, // We wont resave the session variable if nothing is changed
-    saveUninitialized: false
-}))
-app.use(passport.initialize()) 
-app.use(passport.session())
-app.use(methodOverride("_method"))
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
 
 // Configuring the register post functionality
-app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
-    failureFlash: true
-}))
+    failureFlash: true,
+  })
+);
 
 // Configuring the register post functionality
 // After submitting the form, this function will run
-app.post("/register", checkNotAuthenticated, async (req, res) => {
-
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(), 
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
-        })
-        console.log(users); // Display newly registered in the console
-        res.redirect("/login")
-        
-    } catch (e) {
-        console.log(e);
-        res.redirect("/register")
-    }
-})
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log("register request recieved");
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    console.log(users);
+    res.send({ registeredUser: req.body.name });
+    // Display newly registered in the console
+    // res.redirect("/login");
+  } catch (e) {
+    console.log(e);
+    // res.redirect("/register");
+  }
+});
 
 // Routes
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render("index.ejs", {name: req.user.name})
-    
-})
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render("login.ejs")
-})
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render("register.ejs")
-})
-
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
 
 // End Routes
 
@@ -89,24 +95,24 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
 //   })
 
 app.delete("/logout", (req, res) => {
-    req.logout(req.user, err => {
-        if (err) return next(err)
-        res.redirect("/")
-    })
-})
+  req.logout(req.user, (err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
 
-function checkAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    res.redirect("/login")
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
-function checkNotAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return res.redirect("/")
-    }
-    next()
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
 }
 
-app.listen(3000)
+app.listen(5001);
