94b01458958937ef465a7811e28314bc8a515a0b
HarrisonHwang <78999886+HarrisonHwang@users.noreply.github.com>
2023-04-01 09:31:02 -0700

    updated homepage
    decided to do away with modal popups for now b/c managing multiple was a pain in the ass
    
    right now the accounts are displayed through nested accordion menus

--- the-front-end/front-end/src/index.js
+++ the-front-end/front-end/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// this import is necessary for react-boostrap elements
+// ie accordian menu, modal popups, etc...
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
