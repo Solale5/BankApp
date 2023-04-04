a1005624887eee73518e3eb34cb80be423db68ae
AhmedHodhod <alhdhody@gmail.com>
2023-04-03 18:04:29 -0700

    integrated json web tokens

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,8 @@
 const {
   Model
 } = require('sequelize');
+
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -12,13 +14,35 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+
+
   }
+
+
+  User.prototype.generateAuthToken =   async  function () {
+    const user = this
+
+    // change this later to a random string
+    const token = jwt.sign({ _id: user.password}, 'thisismynewcourse')
+    
+    user.tokens = user.tokens.concat({ token })
+
+    return token
+    }
+
+
   User.init({
-    fname: {
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
 
     },
+    password: {
+      type: DataTypes.STRING
+    }
+    , tokens: {
+      type: DataTypes.JSON
+    }
 
   
   }, {
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,23 +1,43 @@
 const express = require('express')
 const {User} = require('../models')
 //const auth = require('../middleware/auth')
+
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
 const router = new express.Router()
-
-
 router.post('/users/signup', async (req, res) => {
 
-  const { fname } = req.body
+  const { email, password } = req.body
 
     try {
         
-      const user = await User.create({ fname })
+      const user = await User.create({ email, password })
       
-      return res.send("signed up")
+      return res.send({user})
 
     } catch (err) {
       console.log(err)
       return res.status(500).json(err)
     }
   })
+
+// copied 
+
+
+router.post('/users/login', async (req, res) => {
+  try {
+      const user = await User.findOne({ where: { email: req.body.email, password:req.body.password } })
+      
+      const token = user.generateAuthToken()
+      res.send({ user, token })
+     
+  } catch (e) {
+      res.status(400).send()
+  }
+})
+
 
 module.exports = router
