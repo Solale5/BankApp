0be10bdb8395f083fddcc520bbed41a030ae363f
Solale5 <solomon.alemu@sjsu.edu>
2023-04-24 21:52:51 -0700

    updated register page

--- the-front-end/front-end/src/pages/register/register.js
+++ the-front-end/front-end/src/pages/register/register.js
@@ -1,6 +1,36 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./register.css";
+let theAge;
+
+// helper function for age validation
+function checkAge(dob) {
+  // Convert the input DOB string to a Date object
+  const dobDate = new Date(dob);
+
+  // Get the current date
+  const currentDate = new Date();
+
+  // Calculate the age by subtracting the year and month of birth from the current year and month
+  let age = currentDate.getFullYear() - dobDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+  const dobMonth = dobDate.getMonth();
+  if (
+    currentMonth < dobMonth ||
+    (currentMonth === dobMonth && currentDate.getDate() < dobDate.getDate())
+  ) {
+    // Reduce the age by 1 if the current month is less than the month of birth,
+    // or if the months are the same but the current day is less than the day of birth
+    age--;
+  }
+  theAge = age;
+  // Check if the age is greater than or equal to 15
+  if (age >= 15) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 function RegistrationPage() {
   const navigate = useNavigate();
@@ -9,13 +39,18 @@ function RegistrationPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
-  const [age, setAge] = useState("");
   const [securityQuestion, setSecurityQuestion] = useState("");
   const [securityAnswer, setSecurityAnswer] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [recoveryEmail, setRecoveryEmail] = useState("");
-  let hardcodedApiKey = "http://localhost:5001/api/clients/signup";
+  const [zipcode, setZipcode] = useState("");
+  const [city, setCity] = useState("");
+  const [street, setStreet] = useState("");
+  const [dob, setDob] = useState("");
+  const [state, setState] = useState("");
 
+  let hardcodedApiKey = "http://localhost:5001/api/clients/signup";
+  const manager = false;
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -23,42 +58,53 @@ function RegistrationPage() {
       alert("Passwords do not match");
       return;
     }
+    console.log(checkAge("2000-03-28"));
+    if (checkAge(dob)) {
+      let age = theAge;
+      try {
+        const response = await fetch(`${hardcodedApiKey}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            password,
+            email,
+            dob,
+            securityQuestion,
+            securityAnswer,
+            phoneNumber,
+            recoveryEmail,
+            zipcode,
+            city,
+            street,
+            state,
+            manager,
+            age,
+          }),
+        });
 
-    try {
-      const response = await fetch(`${hardcodedApiKey}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          password,
-          email,
-          age,
-          securityQuestion,
-          securityAnswer,
-          phoneNumber,
-          recoveryEmail,
-        }),
-      });
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Registration failed");
+        }
 
-      if (!response.ok) {
-        console.log(response);
-        throw new Error("Registration failed");
+        const data = await response.json();
+
+        // Do something with the response data
+
+        console.log(data);
+        navigate("/login");
+        if (data.registeredUser == name) {
+          // window.location.href = "/login";
+        }
+      } catch (error) {
+        console.error(error);
+        // Handle the error
       }
-
-      const data = await response.json();
-
-      // Do something with the response data
-
-      console.log(data);
-      navigate("/login");
-      if (data.registeredUser == name) {
-        // window.location.href = "/login";
-      }
-    } catch (error) {
-      console.error(error);
-      // Handle the error
+    } else {
+      alert("must be 15 or older to register");
     }
   };
   return (
@@ -105,8 +151,8 @@ function RegistrationPage() {
           Birthday:
           <input
             type="date"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
             required
           />
         </label>
@@ -150,6 +196,43 @@ function RegistrationPage() {
             required
           />
         </label>
+        <label>
+          Zip Code:
+          <input
+            type="text"
+            value={zipcode}
+            onChange={(e) => setZipcode(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          City:
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Street:
+          <input
+            type="text"
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          State:
+          <input
+            type="text"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+            required
+          />
+        </label>
+
         <button type="submit">Register</button>
       </form>
     </div>
