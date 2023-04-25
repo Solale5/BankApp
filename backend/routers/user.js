298508b1d5c8ac999ac24037f039a0447dde5630
Solale5 <solomon.alemu@sjsu.edu>
2023-04-25 12:19:11 -0700

    date of birth field

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const { User, Token } = require('../models')
-const auth = require('../middleware/auth')
-const jwt = require('jsonwebtoken')
-const verifyEmail = require('../utils/verifyEmail')
-const Joi = require('joi');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const { User, Token } = require("../models");
+const auth = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+const verifyEmail = require("../utils/verifyEmail");
+const Joi = require("joi");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
-
-const router = new express.Router()
-router.post('/api/clients/signup', async (req, res) => {
+const router = new express.Router();
+router.post("/api/clients/signup", async (req, res) => {
+  console.log("register request recieved");
   try {
     //validate the data
     const schema = Joi.object({
@@ -25,116 +25,124 @@ router.post('/api/clients/signup', async (req, res) => {
       zipcode: Joi.string().length(5).required(),
       street: Joi.string().required(),
       city: Joi.string().required(),
-      state: Joi.string().required()
+      state: Joi.string().required(),
+      dob: Joi.string().required(),
     });
     const { error } = schema.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const userExists = await User.findOne({ where: { email: req.body.email } })
+    const userExists = await User.findOne({ where: { email: req.body.email } });
     if (userExists) {
-      return res.status(400).send('Email already exists')
+      return res.status(400).send("Email already exists");
     }
 
-    const user = await User.create(req.body)
+    const user = await User.create(req.body);
 
-    //verify email 
-    const token = jwt.sign({
-      email: user.email,
-    }, process.env.Private_Key, { expiresIn: '2m' }
+    //verify email
+    const token = jwt.sign(
+      {
+        email: user.email,
+      },
+      process.env.Private_Key,
+      { expiresIn: "2m" }
     );
 
     //await verifyEmail(user, token)
 
-    return res.status(201).send({ user, token })
+    return res.status(201).send({ user, token });
   } catch (err) {
-    return res.status(400).json('Invalid data')
+    return res.status(400).json("Invalid data");
   }
-})
+});
 
 // adding auth only for testing purposes
-router.post('/api/clients/login', async (req, res) => {
+router.post("/api/clients/login", async (req, res) => {
   try {
     //validate the data
     const schema = Joi.object({
       email: Joi.string().email().required(),
       password: Joi.string().min(6).required(),
-      securityAnswer: Joi.string().required()
+      securityAnswer: Joi.string().required(),
     });
     const { error } = schema.validate(req.body);
+    console.log(error);
     if (error) return res.status(400).send(error.details[0].message);
 
     // check the password and the email verification
-    const user = await User.findOne({ where: { email: req.body.email } })
+    const user = await User.findOne({ where: { email: req.body.email } });
     if (!user) {
-      throw new Error()
+      throw new Error();
     }
 
-    // check the password and the security answer 
-    const passwordCheck = await bcrypt.compare(req.body.password, user.password)
-    const securityAnswerCheck = await bcrypt.compare(req.body.securityAnswer, user.securityAnswer)
+    // check the password and the security answer
+    const passwordCheck = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    const securityAnswerCheck = await bcrypt.compare(
+      req.body.securityAnswer,
+      user.securityAnswer
+    );
 
     if (!passwordCheck || !securityAnswerCheck) {
-      throw new Error()
+      throw new Error();
     }
 
     if (!user.active) {
-      await Token.destroy({ where: { userid: user.id } })
-      await verifyEmail(user)
-      return res.status(400).send('Please verify your email to login')
+      await Token.destroy({ where: { userid: user.id } });
+      await verifyEmail(user);
+      return res.status(400).send("Please verify your email to login");
     }
 
     // generate and save the token
-    const token = jwt.sign({ _id: user.id }, process.env.Private_Key)
+    const token = jwt.sign({ _id: user.id }, process.env.Private_Key);
     await Token.create({ userid: user.id, value: token });
 
-    res.status(200).send({ user, token })
-
+    res.status(200).send({ user, token });
   } catch (e) {
-    return res.status(400).send('Invalid login credentials')
+    return res.status(400).send("Invalid login credentials");
   }
-})
+});
 
-// logout 
-router.post('/api/clients/logout', auth, async (req, res) => {
+// logout
+router.post("/api/clients/logout", auth, async (req, res) => {
   try {
     await Token.destroy({
       where: {
         value: req.token,
-        userid: req.user.id
-      }
+        userid: req.user.id,
+      },
     });
-    res.status(200).send()
+    res.status(200).send();
   } catch (e) {
-    res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
-// logout from all devices 
-router.post('/api/clients/logoutAll', auth, async (req, res) => {
+// logout from all devices
+router.post("/api/clients/logoutAll", auth, async (req, res) => {
   try {
     await Token.destroy({
       where: {
-        userid: req.user.id
-      }
+        userid: req.user.id,
+      },
     });
-    res.status(200).send()
+    res.status(200).send();
   } catch (e) {
-    res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
 //get the user profile
-router.get('/api/clients/me', auth, async (req, res) => {
+router.get("/api/clients/me", auth, async (req, res) => {
   try {
-    res.status(200).send(req.user)
+    res.status(200).send(req.user);
+  } catch (e) {
+    res.status(400).send();
   }
-  catch (e) {
-    res.status(400).send()
-  }
-})
+});
 
-router.patch('/api/clients/me', auth, async (req, res) => {
-
+router.patch("/api/clients/me", auth, async (req, res) => {
   //validate the data
   const schema = Joi.object({
     name: Joi.string(),
@@ -144,63 +152,76 @@ router.patch('/api/clients/me', auth, async (req, res) => {
     recoveryEmail: Joi.string().email(),
     street: Joi.string(),
     city: Joi.string(),
-    state: Joi.string()
+    state: Joi.string(),
   });
   const { error } = schema.validate(req.body);
-  if (error) return res.status(400).send('Invalid data');
+  if (error) return res.status(400).send("Invalid data");
 
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ['recoveryEmail', 'street', 'city', 'state', 'name', 'age', 'phoneNumber', 'securityAnswer']
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  const updates = Object.keys(req.body);
+  const allowedUpdates = [
+    "recoveryEmail",
+    "street",
+    "city",
+    "state",
+    "name",
+    "age",
+    "phoneNumber",
+    "securityAnswer",
+  ];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
   if (!isValidOperation) {
-    return res.status(400).send('Invalid updates!')
+    return res.status(400).send("Invalid updates!");
   }
 
   try {
-    updates.forEach((update) => req.user[update] = req.body[update])
-    await req.user.save()
-    res.status(200).send(req.user)
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.status(200).send(req.user);
   } catch (e) {
-    res.status(400).send(e)
+    res.status(400).send(e);
   }
-})
+});
 
-
-router.delete('/api/clients/me', auth, async (req, res) => {
+router.delete("/api/clients/me", auth, async (req, res) => {
   try {
     await Token.destroy({
       where: {
-        userid: req.user.id
-      }
+        userid: req.user.id,
+      },
     });
 
     await User.destroy({
       where: {
-        id: req.user.id
-      }
-    })
-    res.status(200).send()
+        id: req.user.id,
+      },
+    });
+    res.status(200).send();
   } catch (e) {
-    res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
-// verify the email 
-router.post('/api/clients/verify/:token', (req, res) => {
+// verify the email
+router.post("/api/clients/verify/:token", (req, res) => {
   const token = req.params.token;
 
   // Verifying the JWT token
   jwt.verify(token, process.env.Private_Key, async (err, decoded) => {
     if (err) {
-      res.status(400).send("Email verification failed, possibly the link is invalid or expired");
-    }
-    else {
-      const user = await User.findOne({ where: { email: decoded.email } })
-      await user.update({ active: true })
+      res
+        .status(400)
+        .send(
+          "Email verification failed, possibly the link is invalid or expired"
+        );
+    } else {
+      const user = await User.findOne({ where: { email: decoded.email } });
+      await user.update({ active: true });
       res.status(200).send("Email verifified successfully");
     }
   });
 });
 
-module.exports = router
+module.exports = router;
