8e09db7c08511d9b01b13bcec92b811f699ba40a
AhmedHodhod <alhdhody@gmail.com>
2023-05-01 10:24:37 -0700

    removed alter:true option for deployment purposes.

--- backend/app.js
+++ backend/app.js
@@ -122,7 +122,7 @@ const main = async () => {
   // remove the force option to avoid dropping the table
 
   //await sequelize.sync({ alter: true, force: true })
-  await sequelize.sync({ alter: true });
+  await sequelize.sync({ });
 };
 
 main();
